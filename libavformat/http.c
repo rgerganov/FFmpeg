rgerganov/FFmpeg

commit 69fcc093c1241b5ee7711c56c9cd558832a7e491

http: Do not try to make a new request when seeking past the end of the file
This avoids making invalid HTTP Range requests for a byte range past the
known end of the file during a seek. Those requests generally return a HTTP
response of 416 Range Not Satisfiable, which causes an error response.

Reference: https://tools.ietf.org/html/rfc7233

Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

libavformat/http.c
@@ -1691,6 +1691,13 @@ static int64_t http_seek_internal(URLContext *h, int64_t off, int whence, int fo
     if (s->off && h->is_streamed)
         return AVERROR(ENOSYS);
 
+    /* do not try to make a new connection if seeking past the end of the file */
+    if (s->end_off || s->filesize != UINT64_MAX) {
+        uint64_t end_pos = s->end_off ? s->end_off : s->filesize;
+        if (s->off >= end_pos)
+            return s->off;
+    }
+
     /* we save the old context in case the seek fails */
     old_buf_size = s->buf_end - s->buf_ptr;
     memcpy(old_buf, s->buf_ptr, old_buf_size);

commit c4b7260e4115af99914eeffdc2f944e06317b7be

libavformat/http: fix support for reconnect_at_eof
Fix regression introduced with commit 69fcc09 which doesn't honor
reconnect_at_eof and breaks support for live/endless streams. For such
streams we should reconnect at EOF and clients set the reconnect_at_eof
option to signal this.

Signed-off-by: Radoslav Gerganov <rgerganov@gmail.com>

libavformat/http.c
@@ -1988,12 +1988,13 @@ static int64_t http_seek_internal(URLContext *h, int64_t off, int whence, int fo
     if (s->off && h->is_streamed)
         return AVERROR(ENOSYS);
 
-    /* do not try to make a new connection if seeking past the end of the file */
-    if (s->end_off || s->filesize != UINT64_MAX) {
-        uint64_t end_pos = s->end_off ? s->end_off : s->filesize;
-        if (s->off >= end_pos)
-            return s->off;
-    }
+    if (!s->reconnect_at_eof)
+        /* do not try to make a new connection if seeking past the end of the file */
+        if (s->end_off || s->filesize != UINT64_MAX) {
+            uint64_t end_pos = s->end_off ? s->end_off : s->filesize;
+            if (s->off >= end_pos)
+                return s->off;
+        }
 
     /* if the location changed (redirect), revert to the original uri */
     if (strcmp(s->uri, s->location)) {